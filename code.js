de0ea99423b0c4511ca1803edb14f3b2f02411a0
BitLooter <pwnzerdragoon+gh@gmail.com>
2012-11-26 23:57:12 -0800

    Finished writing basic presentation text

--- code.js
+++ code.js
@@ -1,6 +1,7 @@
 function init()
 {
     currentPage = document.getElementById("page0");
+    currentLink = document.getElementById("page0-li");
 
     var linkElements = document.getElementsByClassName("pagelink");
     for (var i = 0; i < linkElements.length; i++) {
@@ -11,6 +12,9 @@ function init()
             var pageId = this.id.split("-")[0];
             currentPage = document.getElementById(pageId);
             currentPage.style.display = "block";
+            currentLink.classList.remove("currentpagelink");
+            this.classList.add("currentpagelink");
+            currentLink = this;
         }
     }
 }
